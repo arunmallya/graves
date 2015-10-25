87cc387f72db0ca4458f1bd4256b611d81c3cfe2
Arun Mallya <arun.mallya@gmail.com>
2015-10-25 15:19:56 -0500

    Add decoder options to RNN & LSTM

--- models/LSTM.lua
+++ models/LSTM.lua
@@ -17,15 +17,18 @@ require 'nngraph'
 
 local LSTM = {}
 
-function LSTM.create(input_size, num_L, num_h, output_size, rnn_type)
+function LSTM.create(input_size, num_L, num_h, opts)
 
     -- check input sanity
-    rnn_type = rnn_type or 'simplified'
+    local opts = opts or {}
+    local rnn_type = opts.rnn_type or 'simplified'
+    local output_size = opts.output_size or -1
+
+    assert(input_size > 0, 'invalid input_size: should be > 0')
+    assert(num_L >= 1, 'invalid num_L: should be >= 1')
     assert(rnn_type == 'simplified' or rnn_type == 'alex', 
         'invalid rnn type: '..rnn_type)
-    assert(input_size > 0, 'invalid input_size: should be > 0')
-    assert(output_size > 0, 'invalid output_size: should be > 0')
-    assert(num_L >= 1, 'invalid num_L: should be >= 1')
+
     if not torch.isTensor(num_h) then 
         num_h = torch.Tensor(num_L):fill(num_h)
     else
@@ -34,6 +37,7 @@ function LSTM.create(input_size, num_L, num_h, output_size, rnn_type)
             a tensor of same length as num_L]])
     end
 
+    -- params are fine, create LSTM
     -- build the required LSTM graph
     if rnn_type == 'simplified' then
         return LSTM.create_simplified(input_size, num_L, num_h, output_size)
@@ -104,11 +108,13 @@ function LSTM.create_simplified(input_size, num_L, num_h, output_size)
         table.insert(outputs, next_h)
     end
 
-    -- set up the decoder
-    local top_h = outputs[#outputs]
-    local proj = nn.Linear(rnn_size, output_size)(top_h):annotate{name='decoder'}
-    local logsoft = nn.LogSoftMax()(proj)
-    table.insert(outputs, logsoft)
+    if output_size ~= -1 then 
+        -- set up the decoder
+        local top_h = outputs[#outputs]
+        local proj = nn.Linear(rnn_size, output_size)(top_h):annotate{name='decoder'}
+        local logsoft = nn.LogSoftMax()(proj)
+        table.insert(outputs, logsoft)
+    end
 
     -- create a module out of the computation graph
     return nn.gModule(inputs, outputs)
@@ -190,11 +196,13 @@ function LSTM.create_alex(input_size, num_L, num_h, output_size)
         table.insert(hidden, next_h)
     end
 
-    -- set up the decoder
-    local top_h = nn.JoinTable(2)(hidden)
-    local proj = nn.Linear(num_hidden, output_size)(top_h):annotate{name='decoder'}
-    local logsoft = nn.LogSoftMax()(proj)
-    table.insert(outputs, logsoft)
+    if output_size ~= -1 then 
+        -- set up the decoder
+        local top_h = nn.JoinTable(2)(hidden)
+        local proj = nn.Linear(num_hidden, output_size)(top_h):annotate{name='decoder'}
+        local logsoft = nn.LogSoftMax()(proj)
+        table.insert(outputs, logsoft)
+    end
 
     -- create a module out of the computation graph
     return nn.gModule(inputs, outputs)
@@ -204,8 +212,11 @@ end
 function LSTM.test()
 
     batch_size = 2
+    opts = {
+        output_size = 500
+    }
 
-    model = LSTM.create(1000, 1, 100, 500)
+    model = LSTM.create(1000, 1, 100)
     dummy_input  = torch.rand(batch_size, 1000)
     dummy_prev_c = torch.rand(batch_size, 100)
     dummy_prev_h = torch.rand(batch_size, 100)
@@ -215,8 +226,17 @@ function LSTM.test()
     print(y)
     graph.dot(model.fg, 'Single Layer LSTM')
 
+    model = LSTM.create(1000, 1, 100, opts)
+    dummy_input  = torch.rand(batch_size, 1000)
+    dummy_prev_c = torch.rand(batch_size, 100)
+    dummy_prev_h = torch.rand(batch_size, 100)
+    y = model:forward({dummy_input, dummy_prev_h, dummy_prev_h})
+    print('Single Layer LSTM Model')
+    print('batch_size: '..batch_size..', layers: 1')
+    print(y)
+    graph.dot(model.fg, 'Single Layer LSTM with Decoder')
 
-    simple_model = LSTM.create(1000, 2, 100, 500)
+    simple_model = LSTM.create(1000, 2, 100, opts)
     dummy_input  = torch.rand(batch_size, 1000)
     dummy_prev_c = torch.rand(batch_size, 100)
     dummy_prev_h = torch.rand(batch_size, 100)
@@ -226,7 +246,8 @@ function LSTM.test()
     print(y)
     graph.dot(simple_model.fg, 'Simple LSTM')
 
-    alex_model = LSTM.create(1000, 2, 100, 500, 'alex')
+    opts.rnn_type = 'alex'
+    alex_model = LSTM.create(1000, 2, 100, opts)
     dummy_input  = torch.rand(batch_size, 1000)
     dummy_prev_c = torch.rand(batch_size, 100)
     dummy_prev_h = torch.rand(batch_size, 100)
--- models/RNN.lua
+++ models/RNN.lua
@@ -14,15 +14,18 @@ require 'nngraph'
 
 local RNN = {}
 
-function RNN.create(input_size, num_L, num_h, output_size, rnn_type)
+function RNN.create(input_size, num_L, num_h, opts)
 
     -- check input sanity
-    rnn_type = rnn_type or 'simplified'
+    local opts = opts or {}
+    local rnn_type = opts.rnn_type or 'simplified'
+    local output_size = opts.output_size or -1
+
+    assert(input_size > 0, 'invalid input_size: should be > 0')
+    assert(num_L >= 1, 'invalid num_L: should be >= 1')
     assert(rnn_type == 'simplified' or rnn_type == 'alex', 
         'invalid rnn type: '..rnn_type)
-    assert(input_size > 0, 'invalid input_size: should be > 0')
-    assert(output_size > 0, 'invalid output_size: should be > 0')
-    assert(num_L >= 1, 'invalid num_L: should be >= 1')
+    
     if not torch.isTensor(num_h) then 
         num_h = torch.Tensor(num_L):fill(num_h)
     else
@@ -31,6 +34,7 @@ function RNN.create(input_size, num_L, num_h, output_size, rnn_type)
             a tensor of same length as num_L]])
     end
 
+    -- params are fine, create RNN
     print('Creating '..rnn_type..' RNN of '..num_L..' layers of size')
     print(num_h)
 
@@ -78,17 +82,19 @@ function RNN.create(input_size, num_L, num_h, output_size, rnn_type)
         table.insert(outputs, next_h)
     end
        
-    -- set up the decoder
-    if rnn_type == 'simplified' then 
-        local top_h = outputs[#outputs]
-        local proj = nn.Linear(rnn_size, output_size)(top_h):annotate{name='decoder'}
-        local logsoft = nn.LogSoftMax()(proj)
-        table.insert(outputs, logsoft)
-    elseif rnn_type == 'alex' then
-        local top_h = nn.JoinTable(2)(outputs)
-        local proj = nn.Linear(num_hidden, output_size)(top_h):annotate{name='decoder'}
-        local logsoft = nn.LogSoftMax()(proj)
-        table.insert(outputs, logsoft)
+    if output_size ~= -1 then
+        -- set up the decoder
+        if rnn_type == 'simplified' then 
+            local top_h = outputs[#outputs]
+            local proj = nn.Linear(rnn_size, output_size)(top_h):annotate{name='decoder'}
+            local logsoft = nn.LogSoftMax()(proj)
+            table.insert(outputs, logsoft)
+        elseif rnn_type == 'alex' then
+            local top_h = nn.JoinTable(2)(outputs)
+            local proj = nn.Linear(num_hidden, output_size)(top_h):annotate{name='decoder'}
+            local logsoft = nn.LogSoftMax()(proj)
+            table.insert(outputs, logsoft)
+        end
     end
 
     -- create a module out of the computation graph
@@ -100,8 +106,11 @@ function RNN.test()
 -- create RNN, do a forward pass and plot the forward computation graph
 
     batch_size = 2
+    opts = {
+        output_size = 500
+    }
 
-    model = RNN.create(1000, 1, 100, 500)
+    model = RNN.create(1000, 1, 100)
     dummy_input = torch.rand(batch_size, 1000)
     dummy_prev  = torch.rand(batch_size, 100)
     y = model:forward({dummy_input, dummy_prev})
@@ -110,7 +119,16 @@ function RNN.test()
     print(y)
     graph.dot(model.fg, 'Single Layer RNN')
 
-    simple_model = RNN.create(1000, 2, 100, 500)
+    model = RNN.create(1000, 1, 100, opts)
+    dummy_input = torch.rand(batch_size, 1000)
+    dummy_prev  = torch.rand(batch_size, 100)
+    y = model:forward({dummy_input, dummy_prev})
+    print('Single Layer RNN with Decoder Model')
+    print('batch_size: '..batch_size..', layers: 1')
+    print(y)
+    graph.dot(model.fg, 'Single Layer RNN with Decoder')
+
+    simple_model = RNN.create(1000, 2, 100, opts)
     dummy_input = torch.rand(batch_size, 1000)
     dummy_prev  = torch.rand(batch_size, 100)
     y = simple_model:forward({dummy_input, dummy_prev, dummy_prev})
@@ -119,7 +137,8 @@ function RNN.test()
     print(y)
     graph.dot(simple_model.fg, 'Simple RNN')
 
-    alex_model = RNN.create(1000, 2, torch.Tensor{100, 200}, 500, 'alex')
+    opts.rnn_type = 'alex'
+    alex_model = RNN.create(1000, 2, torch.Tensor{100, 200}, opts)
     dummy_input = torch.rand(batch_size, 1000)
     dummy_prev1 = torch.rand(batch_size, 100)
     dummy_prev2 = torch.rand(batch_size, 200)
